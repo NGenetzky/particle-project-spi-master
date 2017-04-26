24627736f76019baf4e62225944973f80241f435
ngenetzky <N@Genetzky.us>
2017-04-26 11:48:01 +0000

    spi_master: Added transfer(vec<uint8_t>) and dr/ir shifts vec.

--- src/JtagDevice.h
+++ src/JtagDevice.h
@@ -1,6 +1,7 @@
 
 #pragma once
 #include "stdint.h"
+#include "ParticleSpi.h"
 
 struct JtagDevice {
     const unsigned cs_jtag = A3;
@@ -15,6 +16,24 @@ struct JtagDevice {
         pinMode(this->cs_jtag, OUTPUT);
     }
     
+    void beginTransaction(bool dr=true)
+    {
+        if(dr){
+            this->tap.goto_state(JtagTapState::SHIFT_DR);
+        } else {
+            this->tap.goto_state(JtagTapState::SHIFT_IR);
+        }
+        digitalWrite(this->cs_jtag, HIGH);
+        SPI.beginTransaction(this->settings);
+    }
+    
+    void endTransaction(bool goto_rti=true)
+    {
+        SPI.endTransaction();
+        digitalWrite(this->cs_jtag, LOW);
+        this->tap.goto_state(JtagTapState::RUN_TEST_IDLE);
+    }
+    
     uint8_t transfer(uint8_t tx){
         digitalWrite(this->cs_jtag, HIGH);
         SPI.beginTransaction(this->settings);
@@ -24,17 +43,20 @@ struct JtagDevice {
         return rx;
     }
     
-    uint8_t ir_shift(uint8_t value){
-        this->tap.goto_state(JtagTapState::SHIFT_IR);
+    // uint8_t
+    uint8_t ir_shift(uint8_t value)
+    {
+        this->beginTransaction(false);
         auto rv = this->transfer(value);
-        this->tap.goto_state(JtagTapState::RUN_TEST_IDLE);
+        this->endTransaction(true);
         return rv;
     }
     
-    uint8_t dr_shift(uint8_t value){
-        this->tap.goto_state(JtagTapState::SHIFT_DR);
-        auto rv = this->transfer(value);
-        this->tap.goto_state(JtagTapState::RUN_TEST_IDLE);
+    uint8_t dr_shift(uint8_t value)
+    {
+        this->beginTransaction(true);
+        auto rv = SPI.transfer(value);
+        this->endTransaction(true);
         return rv;
     }
     
@@ -45,6 +67,22 @@ struct JtagDevice {
     // { }
     
     // uint32_t ir_shift( uint32_t new_ir ){ }
+    
+    std::vector<uint8_t> ir_shift(std::vector<uint8_t> tx_buffer)
+    {
+        this->beginTransaction(false);
+        auto rx_buffer = spi_transfer(tx_buffer);
+        this->endTransaction(true);
+        return rx_buffer;
+    }
+    
+    std::vector<uint8_t> dr_shift(std::vector<uint8_t> tx_buffer)
+    {
+        this->beginTransaction(true);
+        auto rx_buffer = spi_transfer(tx_buffer);
+        this->endTransaction(true);
+        return rx_buffer;
+    }
 
     
 };
